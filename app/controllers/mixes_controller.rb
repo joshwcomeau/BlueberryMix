52167e8d0a8cd889173274f7e33426b0e34b8506
Josh Comeau <joshwcomeau@gmail.com>
2014-08-24 14:41:07 -0400

    Added an index of all bucket contents in Index. Started playing with S3 methods

--- app/controllers/mixes_controller.rb
+++ app/controllers/mixes_controller.rb
@@ -1,5 +1,17 @@
 class MixesController < ApplicationController
   def index
+    @mixes = AWS::S3::Bucket.find(ENV["S3_BUCKET"]).objects
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
 
   end
 end
