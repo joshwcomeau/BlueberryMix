f272bdc25cd79d391204ae288c2a43c2e49f57e8
Josh Comeau <joshwcomeau@gmail.com>
2014-08-24 15:07:58 -0400

    Working upload! Making good progress here.

--- Gemfile
+++ Gemfile
@@ -7,13 +7,15 @@ gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
-gem 'sdoc', '~> 0.4.0',          group: :doc
-gem 'spring',        group: :development
+gem 'sdoc', '~> 0.4.0',       group: :doc
+gem 'spring',                 group: :development
 
 # Custom gems
 gem 'devise'
 gem 'quiet_assets'
 gem 'figaro'
 gem 'omniauth'
-gem 'aws-s3', require: 'aws/s3'
+gem 'aws-s3',     require: 'aws/s3'
 gem 'annotate'
+gem 'pry',        group: :development
+gem 'pry-byebug', group: :development 
--- app/controllers/mixes_controller.rb
+++ app/controllers/mixes_controller.rb
@@ -1,6 +1,9 @@
 class MixesController < ApplicationController
+  before_action :get_bucket
+
   def index
-    @mixes = AWS::S3::Bucket.find(ENV["S3_BUCKET"]).objects
+    @mixes = AWS::S3::Bucket.find(@bucket).objects
+
   end
 
   def new
@@ -8,10 +11,40 @@ class MixesController < ApplicationController
   end
 
   def create
+    filename = sanitize_filename(params[:mix].original_filename)
+    if AWS::S3::S3Object.store(filename, params[:mix].read, @bucket, :access => :public_read)
+      redirect_to root_path
+    else
+      render :new, alert: "Upload failed"
+    end
+
 
   end
 
   def destroy
+    if (params[:id])
+      filename = params[:id] + "." + params[:format]
+      AWS::S3::S3Object.find(filename, @bucket).delete
+      redirect_to root_path, notice: "File deleted!"
+    else
+      render text: "No song was found to delete!"
+    end
+  end
 
+  private
+
+  def get_bucket
+    @bucket = ENV["S3_BUCKET"]
+  end
+
+  def sanitize_filename(filename)
+    returning filename.strip do |name|
+     # NOTE: File.basename doesn't work right with Windows paths on Unix
+     # get only the filename, not the whole path
+     name.gsub!(/^.*(\\|\/)/, '')
+
+     # Strip out the non-ascii character
+     name.gsub!(/[^0-9A-Za-z.\-]/, '_')
+    end
   end
 end
